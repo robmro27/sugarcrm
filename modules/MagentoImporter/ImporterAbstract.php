00e92e3cd620511acf859670637962f60b69a394
robmro27 <robert.mroz1985@gmail.com>
2016-05-17 11:48:03 +0200

    polcode caredential

--- modules/MagentoImporter/ImporterAbstract.php
+++ modules/MagentoImporter/ImporterAbstract.php
@@ -26,7 +26,7 @@ class ImporterAbstract {
      * Url's of Magento instance 
      */
     const API_URL_LOCAL = 'http://sugarcrm_magento.local/api/V2_soap/?wsdl=1';
-    const API_URL = 'http://polcode:polcode@magentosugarcrm.rmroz.sites.polcode.net/index.php/api/V2_soap/?wsdl=1';
+    const API_URL = 'http://magentosugarcrm.rmroz.sites.polcode.net/index.php/api/V2_soap/?wsdl=1';
     
     /**
      * Caredentials
@@ -54,7 +54,7 @@ class ImporterAbstract {
     public function __construct() {
         
         if ( $this->soapClient == null || $this->sessionId == null ) {
-            $this->soapClient = new SoapClient($this->getApiUrl());
+            $this->soapClient = new SoapClient($this->getApiUrl(), array('login' => 'polcode', 'password' => 'polcode'));
             $this->sessionId = $this->soapClient->login(self::USERNAME, self::PASSWORD);
         }
         
